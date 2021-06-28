a6cdc3ae5180a470f73882bc4b1e16fcdce880be
jayjay <jinwoolee131@gmail.com>
2021-06-28 13:53:18 +0900

    final

--- run.py
+++ run.py
@@ -45,5 +45,15 @@ for i in range(n):
     t.right((360 / n) * 2)
     t.forward(100)
     t.left(360 / n)
+t.clear()
+
+n, line = map(int, input().split())
+t.shape('turtle')
+t.speed('fastest')
+for i in range(n):
+    t.forward(line)
+    t.right((360 / n) * 2)
+    t.forward(line)
+    t.left(360 / n)
 
 t.mainloop()
