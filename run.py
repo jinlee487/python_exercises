0aa3ba57009a600651d87d23de9adacded312a59
jayjay <jinwoolee131@gmail.com>
2021-06-28 13:43:28 +0900

    final

--- run.py
+++ run.py
@@ -11,12 +11,39 @@ t.fd(100)
 
 n = 6   
 t.color('#FF69B4')    
-t.shape('turtle')
 t.color('red')         
 t.begin_fill()          
 for i in range(n):      
     t.forward(100)
     t.right(360 / n)  
 t.end_fill()          
+
+t.clear()
+
+t.circle(120)
+t.clear()
+
+n = 60    # 원을 60번 그림
+t.speed('fastest')      # 거북이 속도를 가장 빠르게 설정
+for i in range(n):
+    t.circle(120)       # 반지름이 120인 원을 그림
+    t.right(360 / n)    # 오른쪽으로 6도 회전
+
+t.clear()
+
+t.shape('arrow')    # 화살표 모양 사용
+for i in range(300):    # 300번 반복
+    t.forward(i)        # i만큼 앞으로 이동. 반복할 때마다 선이 길어짐
+    t.right(91)         # 오른쪽으로 91도 회전
+
+t.clear()
+
+n = 5
+t.shape('turtle')
+for i in range(n):
+    t.forward(100)
+    t.right((360 / n) * 2)
+    t.forward(100)
+    t.left(360 / n)
 
 t.mainloop()
