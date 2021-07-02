bcbfd24f6b770ff5c7c65f07e86e3ce33cc4ab74
jayjay <jinwoolee131@gmail.com>
2021-07-02 12:19:29 +0900

    final

--- run.py
+++ run.py
@@ -2,12 +2,9 @@
 # words.txt 파일에서 문자 c가 포함된 단어를 각 줄에 출력하는 프로그램을 만드세요.
 # 단어를 출력할 때는 등장한 순서대로 출력해야 하며 ,(콤마)와 .(점)은 출력하지 않아야 합니다.
 
-with open('words.txt', 'r') as file:
-    words = file.readlines()
-    for word in words:
-        word = word.strip("\n")
-        if word == word[::-1]:
-            print(word)
+x, y = map(int, input().split())
+def calc(x,y):
+    return x+y, x-y, x*y,x/y
 
-
-    
+a, s, m, d = calc(x, y)
+print('덧셈: {0}, 뺄셈: {1}, 곱셈: {2}, 나눗셈: {3}'.format(a, s, m, d))
