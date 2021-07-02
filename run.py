d2c1cb19af1ee2d2bbb8790b6ae610fbf7071852
jayjay <jinwoolee131@gmail.com>
2021-07-02 14:25:29 +0900

    final

--- run.py
+++ run.py
@@ -2,9 +2,24 @@
 # words.txt 파일에서 문자 c가 포함된 단어를 각 줄에 출력하는 프로그램을 만드세요.
 # 단어를 출력할 때는 등장한 순서대로 출력해야 하며 ,(콤마)와 .(점)은 출력하지 않아야 합니다.
 
-x, y = map(int, input().split())
-def calc(x,y):
-    return x+y, x-y, x*y,x/y
+korean, english, mathematics, science = map(int, input().split())
 
-a, s, m, d = calc(x, y)
-print('덧셈: {0}, 뺄셈: {1}, 곱셈: {2}, 나눗셈: {3}'.format(a, s, m, d))
+def get_min_max_score(*args):
+    return min(*args),max(*args)
+def get_average(**kwargs):
+    return sum(kwargs.values())/len(kwargs)
+
+
+
+
+
+min_score, max_score = get_min_max_score(korean, english, mathematics, science)
+average_score = get_average(korean=korean, english=english,
+                            mathematics=mathematics, science=science)
+print('낮은 점수: {0:.2f}, 높은 점수: {1:.2f}, 평균 점수: {2:.2f}'
+      .format(min_score, max_score, average_score))
+ 
+min_score, max_score = get_min_max_score(english, science)
+average_score = get_average(english=english, science=science)
+print('낮은 점수: {0:.2f}, 높은 점수: {1:.2f}, 평균 점수: {2:.2f}'
+      .format(min_score, max_score, average_score))
