c833dbead8e675aa7a60550e525ede52def0f21d
jayjay <jinwoolee131@gmail.com>
2021-07-05 13:56:46 +0900

    final

--- run.py
+++ run.py
@@ -1,25 +1,14 @@
-# 문자열이 저장된 words.txt 파일이 주어집니다(문자열은 한 줄로 저장되어 있습니다). 
-# words.txt 파일에서 문자 c가 포함된 단어를 각 줄에 출력하는 프로그램을 만드세요.
-# 단어를 출력할 때는 등장한 순서대로 출력해야 하며 ,(콤마)와 .(점)은 출력하지 않아야 합니다.
-
-korean, english, mathematics, science = map(int, input().split())
-
-def get_min_max_score(*args):
-    return min(*args),max(*args)
-def get_average(**kwargs):
-    return sum(kwargs.values())/len(kwargs)
-
+# 표준 입력으로 숫자.
+# 확장자 형식으로 된 파일 이름 여러 개가 입력됩니다. 
+# 다음 소스 코드를 완성하여 파일 이름이 숫자 3개이면서 
+# 앞에 0이 들어가는 형식으로 출력되게 만드세요. 
+# 예를 들어 1.png는 001.png, 99.docx는 099.docx, 
+# 100.xlsx는 100.xlsx처럼 출력되어야 합니다. 
+# 그리고 람다 표현식을 사용해야 하며 출력 결과는 
+# 리스트 형태라야 합니다. 람다 표현식에서 파일명을 
+# 처리할 때는 문자열 포매팅과 문자열 메서드를 활용하세요.
 
 
+files = input().split()
 
-
-min_score, max_score = get_min_max_score(korean, english, mathematics, science)
-average_score = get_average(korean=korean, english=english,
-                            mathematics=mathematics, science=science)
-print('낮은 점수: {0:.2f}, 높은 점수: {1:.2f}, 평균 점수: {2:.2f}'
-      .format(min_score, max_score, average_score))
- 
-min_score, max_score = get_min_max_score(english, science)
-average_score = get_average(english=english, science=science)
-print('낮은 점수: {0:.2f}, 높은 점수: {1:.2f}, 평균 점수: {2:.2f}'
-      .format(min_score, max_score, average_score))
+print(list(map(lambda x : '{0:03d}'.format(int(x.split('.')[0]))+ '.' + x.split('.')[1], files)))
