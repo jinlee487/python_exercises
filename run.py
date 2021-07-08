5309aa475d89a04f4a5d8d1ed0b71eac9158f4c4
jayjay <jinwoolee131@gmail.com>
2021-07-08 14:08:55 +0900

    update

--- run.py
+++ run.py
@@ -1,22 +1,27 @@
-# 다음 소스 코드에서 동물 클래스 Animal과 날개 클래스 Wing을 상속받아
-# 새 클래스 Bird를 작성하여 
-# '먹다', '파닥거리다', '날다', True, True가 각 줄에 출력되게 만드세요.
+# 표준 입력으로 x, y 좌표 4개가 입력되어 
+# Point2D 클래스의 인스턴스 리스트에 저장됩니다. 
+# 여기서 점 4개는 첫 번째 점부터 마지막 점까지 순서대로 이어져 있습니다. 
+# 다음 소스 코드를 완성하여 
+# 첫 번째 점부터 마지막 점까지 연결된 선의 길이가 출력되게 만드세요.
+import math
+import collections
 
-class Animal:
-    def eat(self):
-        print('먹다')
+
+
+
+class Point2D:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
  
-class Wing:
-    def flap(self):
-        print('파닥거리다')
+length = 0.0
+p = [Point2D(), Point2D(), Point2D(), Point2D()]
+p[0].x, p[0].y, p[1].x, p[1].y, p[2].x, p[2].y, p[3].x, p[3].y = map(int, input().split())
 
-class Bird(Animal,Wing):
-    def fly(self):
-        print('날다')
+for i in range(1,len(p)):
+    a = p[i-1].x - p[i].x    # 선 a의 길이
+    b = p[i-1].y - p[i].y    # 선 b의 길이
+    length += math.sqrt((a * a) + (b * b))
 
-b = Bird()
-b.eat()
-b.flap()
-b.fly()
-print(issubclass(Bird, Animal))
-print(issubclass(Bird, Wing))
+
+print(length)
